23107aa79abb8a4ad0d4dca9100ae3651ce17602
Ellie <ellie@lly.dev>
2025-05-16 19:42:47 -0500

    beginning of game loop

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    colors = {
+        "black": pygame.Color(0, 0, 0)
+    }
+    while pygame.get_init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(colors["black"])
+
 if __name__ == "__main__":
     main()
