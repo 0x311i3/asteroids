ac689b5891f1f0bc14ef7ad4ba8808e6a1f13a82
Ellie <ellie@lly.dev>
2025-05-16 19:44:27 -0500

    removed color dict (already provided by lib)

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    colors = {
-        "black": pygame.Color(0, 0, 0)
-    }
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(colors["black"])
+        screen.fill("black")
 
 if __name__ == "__main__":
     main()
